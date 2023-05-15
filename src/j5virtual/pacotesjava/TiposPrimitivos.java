963ec0531fa1a43c2df46151851b897fce682e4a
juniordeveloper11 <juniordeveloperbr@gmail.com>
2023-05-15 20:55:30 -0300

    Initial commit

---
+++ src/j5virtual/pacotesjava/TiposPrimitivos.java
@@ -0,0 +1,20 @@
+package j5virtual.pacotesjava;
+
+public class TiposPrimitivos {
+    public static void main(String[] args) {
+        int idade = 48;
+        long numeroGrande = 10000000;
+        double salarioDouble = 2800;
+        float salarioFloat = 2500;
+        byte  idadeByte = 48;
+        short idadeShort = 48;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 'm';
+
+        System.out.println(numeroGrande);
+
+
+        System.out.println(" idade"+ idade + "anos");
+    }
+}
---
+++ src/j5virtual/pacotesjava/pacoteJava.java
@@ -0,0 +1,11 @@
+package j5virtual.pacotesjava;
+
+/**
+ * isso é um comentario javadoc
+ */
+public class pacoteJava {
+
+    public static void main(String[] args) {
+
+    }
+}
